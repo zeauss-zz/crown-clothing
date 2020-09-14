596447e6c3a3071eac181bf2a4ed14ad84117eef
unknown <jose@pippa.com.br>
2020-09-14 13:00:43 -0300

    shop page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './App.css';
 
 import ShopPage from './pages/shop/shop.component'
-
 import { Switch, Route } from 'react-router-dom'
-
 import HomePage from './pages/homepage/homepage.component'
+
+import Header from './components/header/header.component'
 
 function App() {
   return (
     <div>
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
-    </Switch>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
